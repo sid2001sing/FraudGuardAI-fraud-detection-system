62a7efe0ad8b5f52eba88ebac7e26e1514d68737
sid2001sing <148205172+sid2001sing@users.noreply.github.com>
2025-11-21 16:26:55 +0530

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FraudDetector from './FraudDetector';
+
+function App() {
+  return (
+    <div>
+      <FraudDetector />
+    </div>
+  );
+}
+
+export default App;
